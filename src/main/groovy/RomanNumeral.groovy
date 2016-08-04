53d235491b14f0d2416039850bb51ba5337beae7
David Bierce <david@bierce.org>
2016-08-03 20:47:48 -0500

    Adding test for 1989 showed a large flaw in how I was generating the final string using a closure.  Results are now generated to a StringBuilder by using a looking up a list.  The list is pruned each run of the highest possible value then decremented.  String manipulation like this feels dirty but I guess I can't go back in time and tell the Romans how to math.

--- src/main/groovy/RomanNumeral.groovy
+++ src/main/groovy/RomanNumeral.groovy
@@ -1,19 +1,19 @@
 class RomanNumeral {
 
-    static final Map<Integer, String> NUMERALS = [
+    static final Map<Integer, String> NUMERALS_TABLE = [
             1000: "M",
-            900: "CM",
-            500: "D",
-            400: "CD",
-            100: "C",
-            90: "XC",
-            50: "L",
-            40: "XL",
-            10: "X",
-            9: "IX",
-            6: "VI",
-            4: "IV",
-            1: "I"
+            900 : "CM",
+            500 : "D",
+            400 : "CD",
+            100 : "C",
+            90  : "XC",
+            50  : "L",
+            40  : "XL",
+            10  : "X",
+            9   : "IX",
+            6   : "VI",
+            4   : "IV",
+            1   : "I"
     ]
 
     /**
@@ -24,20 +24,25 @@ class RomanNumeral {
      * */
     String toRomanNumeral(int input) {
         if (input < 1) throw new IllegalArgumentException("Input ${input} is out of range.  Must be greater than 0")
-        String result = ""
-
+        StringBuilder result = new StringBuilder()
+        /**
+         * Create a list of keys editable for later
+         **/
+        List numberList = NUMERALS_TABLE.keySet() as List
         while (input > 0) {
             /**
-             * Iterate over conversion map from big to to small
-             * Decrementing for the Numeral value until zero
+             * Before comparing remove any numbers higher the current input
              */
-            NUMERALS.each { number, numeral ->
-                if (input >= number) {
-                    result <<= numeral
-                    input = input - number
-                }
-            }
+            numberList.removeAll { it > input }
+
+            /**
+             * Choose the highest remaining mapped value in the list
+             * Use the value to check against conversion map and decrement input
+             */
+            int remainder = numberList.max()
+            result << NUMERALS_TABLE[remainder]
+            input = input - remainder
         }
-        return result
+        result
     }
 }
--- src/test/groovy/RomanNumeralTest.groovy
+++ src/test/groovy/RomanNumeralTest.groovy
@@ -36,7 +36,7 @@ class RomanNumeralTest extends Specification {
         romanNumeral.toRomanNumeral(input) == result
 
         where:
-        input << [1, 3, 4, 6, 7, 9, 10, 14, 40, 44, 50, 90, 94, 100, 400, 500, 1066]
-        result << ["I", "III", "IV", "VI", "VII", "IX", "X", "XIV", "XL", "XLIV", "L", "XC", "XCIV", "C", "CD", "D", "MLXVI"]
+        input << [1, 3, 4, 6, 7, 9, 10, 14, 40, 44, 50, 90, 94, 100, 400, 500, 1066, 1993, 1989 ]
+        result << ["I", "III", "IV", "VI", "VII", "IX", "X", "XIV", "XL", "XLIV", "L", "XC", "XCIV", "C", "CD", "D", "MLXVI", "MCMXCIII", "MCMLXXXIX"]
     }
 }
